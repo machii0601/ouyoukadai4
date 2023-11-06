0c7d154ae5261768180ddd32ae903c110dfa47a9
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-11-06 10:41:58 +0000

    [Add]ビューを表示

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,33 @@
+class SearchesController < ApplicationController
+  def search
+    @search_target = params[:search_target]
+    @search_method = params[:search_method]
+    @query = params[:query]
+    if @search_target == "posts"
+      case @search_method
+        when "exact"
+          @results = Book.where(title: @query)
+        when "prefix"
+          @results = Book.where("title LIKE ?", "#{@query}%")
+        when "suffix"
+          @results = Book.where("title LIKE ?", "%#{@query}")
+        when "partial"
+          @results = Book.where("title LIKE ?", "%#{@query}%")
+      else
+      end
+    elsif @search_target == "users"
+      case @search_method
+        when "exact"
+          @results = User.where("name": @query)
+        when "prefix"
+          @results = User.where("name LIKE ?", "#{@query}%")
+        when "suffix"
+          @results = User.where("name LIKE ?", "%#{@query}")
+        when "partial"
+          @results = User.where("name LIKE ?", "%#{@query}%")
+        else
+      end
+    end
+
+  end
+end
